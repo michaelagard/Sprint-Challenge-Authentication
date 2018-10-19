528b5fe31301d951d258c0dedb58c9780cf7285c
michaelagard <mdagard@gmail.com>
2018-10-19 12:09:07 -0400

    add db, bcrypt, jwt and jwtKey endpoints for register and login implemented and work. getJokes doesn't

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig')
 const { authenticate } = require('./middlewares');
-
+const jwtKey = require('../_secrets/keys').jwtKey;
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
@@ -9,11 +11,43 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 10);
+  credentials.password = hash;
+  // console.log(credentials)
+  db('users')
+    .insert(credentials)
+    .then(ids => {
+      const id = ids[0];
+      const token = generateToken({ username: credentials.username });
+      res.status(201).json({ newUserId: id, token });
+      console.log('did is succeed?')
+      console.log(token)
+    })
+    .catch(err => {
+      console.log('is this the error?')
+      res.status(500).json(err);
+      console.log(err)
+    });
 }
 
 function login(req, res) {
-  // implement user login
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ welcome: user.username, token });
+      } else {
+        res.status(401).json({ message: 'you shall not pass!' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
 }
 
 function getJokes(req, res) {
@@ -27,4 +61,16 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+function generateToken(user) {
+  const jwtPayload = {
+    ...user,
+  };
+  const jwtOptions = {
+    expiresIn: '1m',
+  };
+
+  console.log('token from process.env', jwtKey);
+  return jwt.sign(jwtPayload, jwtKey, jwtOptions);
 }
