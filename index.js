7ed356d0e191d88e23931fd04c1b850bb0a68c3c
michaelagard <mdagard@gmail.com>
2018-10-19 12:08:16 -0400

    add sanity check and few variables for convenience

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const { server } = require('./server.js');
+const projectPullRequest = 'https://github.com/LambdaSchool/Sprint-Challenge-Authentication/pull/390';
+const serverPort = '7100';
+const serverName = 'Sprint-Challenge-Authentication';
+const port = process.env.PORT || serverPort;
 
-const port = process.env.PORT || 3300;
+server.get('/', (req, res) => { // sanity check root endpoint
+  res.send(`${serverName} running on port ${serverPort}<br>More information: <a href="${projectPullRequest}">GitHub Repo</a>`);
+});
+
 server.listen(port, () => {
   console.log(`\n=== Server listening on port ${port}\n`);
 });
